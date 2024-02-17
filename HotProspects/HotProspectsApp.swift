7a1c3111efd9f21723ce2fc3c705c97c3bc3f1bb
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-02-17 21:40:33 +0600

    project init

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Shah Md Imran Hossain on 17/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
