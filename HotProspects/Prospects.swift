7227428f8968ec4019724f05e5a5dfb0faa963d0
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-02-17 21:54:15 +0600

    add prospects

---
+++ HotProspects/Prospects.swift
@@ -0,0 +1,52 @@
+//
+//  Prospects.swift
+//  HotProspects
+//
+//  Created by Shah Md Imran Hossain on 17/2/24.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+@MainActor
+class Prospects: ObservableObject {
+    @Published private(set) var people: [Prospect]
+    let saveKey = "SavedData"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        // no saved data!
+        people = []
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        // need to do it before, before the changes not after the changes
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+}
+
